6a4fae99b68ce6a4955e3f276511cddcaccfc609
2or3icecubes <com9on@me.com>
2021-12-03 15:50:46 +0100

    Clean code from termi.py

--- termi.py
+++ termi.py
@@ -1,7 +1,26 @@
 
+class pet_status:
+    level: int
+    health: int
+    mana: int
+    hungry: int
+
+    def __init__(self):
+        pass
+
+
 class TermiGotchi:
-    name: String
-    status: PetStatus   
+    name: str
+    status: pet_status
+
+    def print_status(self):
+        pass
+
+    def print(self):
+        pass
+
+
+
 
 def main() -> None:
     print("Termi Gotchi Class")
